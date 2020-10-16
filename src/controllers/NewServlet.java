56957402b9e1cc1d11854327b9b46f82117e776b
haruna <haruteru9924@icloud.com>
2020-10-16 18:42:43 +0900

    Add NewServlet

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,19 +1,25 @@
 package controllers;
 
 import java.io.IOException;
+import java.sql.Timestamp;
+
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.TaskDTO;
+import utils.DBUtil;
+
 /**
  * Servlet implementation class NewServlet
  */
 @WebServlet("/new")
 public class NewServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -22,12 +28,27 @@ public class NewServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+        em.getTransaction().begin();
 
+        TaskDTO m = new TaskDTO();
+
+        String content = "newtask";
+        m.setContent(content);
+
+        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+        m.setCreated_at(currentTime);
+        m.setUpdated_at(currentTime);
+
+        em.persist(m);
+        em.getTransaction().commit();
+
+        response.getWriter().append(Integer.valueOf(m.getId()).toString());
+
+        em.close();
+    }
 }
